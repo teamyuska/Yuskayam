46f2688b97ad16cafd9effd5df2b5a48e7760cb3
teamyuska <118346503+teamyuska@users.noreply.github.com>
2023-12-19 08:38:38 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-# Zaute Km <https://t.me/ZauteKm>
-
-import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = os.getenv("API_ID", "7948888")
+API_HASH = os.getenv("API_HASH", "48285ac512613754f7913b310e884222")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "AAEqr0qHkncbPANl4sIyY3Zsrz08qg88tkc")
+DATABASE_URL = os.getenv("DATABASE_URL")
+OWNER_ID = os.getenv("OWNER_ID", "6033604536")
